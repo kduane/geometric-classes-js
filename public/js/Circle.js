9b09efbd4f1a9271a9cc60767952a56b833687ef
kduane <karl.duane@gmail.com>
2018-06-09 16:26:06 -0400

    diameter function defined, now for perimeter

--- public/js/Circle.js
+++ public/js/Circle.js
@@ -1,3 +1,20 @@
 class Circle {
+  constructor(radius, x, y) {
+    this.radius = radius;
+      this.x = x || 0;
+      this.y = y || 0;
 
+    }
+
+    area() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    diameter() {
+      return 2 * this.radius;
+    }
+
+    perimeter() {
+      return 
+    }
 }
