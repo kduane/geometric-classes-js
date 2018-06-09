6e31c154853abc138ee7e95ab5559b12dc99abf6
kduane <karl.duane@gmail.com>
2018-06-09 15:56:38 -0400

    first commit

---
+++ public/js/spec/SquareSpec.js
@@ -0,0 +1,37 @@
+describe('Square', () =>  {
+  describe('new Square()', () =>  {
+    it('takes argument for a length of a side', () =>  {
+
+    });
+
+    it('takes optional argument for the center point', () =>  {
+
+    });
+
+    it('defaults (x,y) to (0,0) if not provided', () =>  {
+
+    });
+  });
+
+  describe('area()', () =>  {
+    it('returns the area of the square', () =>  {
+
+    });
+  });
+
+  describe('perimeter()', () =>  {
+    it('returns the perimeter of the square', () =>  {
+
+    });
+  });
+
+  describe('contains_point?(x,y)', () =>  {
+    it('returns true for a point given inside of the square\'s perimeter', () => {
+
+    });
+
+    it('returns false for a point given outside of the square\'s perimeter', () => {
+
+    });
+  });
+});
